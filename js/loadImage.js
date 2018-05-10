144cd49a65cd7432d3d629cdc6da26c12c537089
Nicola <pagiaro@gmail.com>
2018-05-10 10:13:57 +0200

    messo caricamento

---
+++ js/loadImage.js
@@ -0,0 +1,9 @@
+const images = document.getElementsByClassName('load-image')
+for(let i=0; i < images.length; i++) {
+  let img = new Image()
+  img.src = images[i].dataset.image;
+  img.onload = function() {
+    images[i].style.backgroundImage = "url('" + img.src + "')"
+    console.log(images[i].style.backgroundImage);
+  }
+}
--- js/prog_script.js
+++ js/prog_script.js
@@ -9,24 +9,3 @@ const specsCard = document.getElementsByClassName('card-descr')[0]
  * Animazione di fade in di entrata
  */
 fadeIn(container)
-
-/* click listener sulla card
-const cards = document.getElementsByClassName("card")
-for(let i=0; i < cards.length; i++) {
-  cards[i].addEventListener('click', () => {
-    bgDark.style.display = "block"
-    setTimeout(()=> {
-      specsCard.style.width = "500px"
-      specsCard.style.height = "500px"
-    }, 50)
-  })
-}
-
-// click listener sullo sfondo nero
-bgDark.addEventListener('click', () => {
-  specsCard.style.width = "0px"
-  specsCard.style.height = "0px"
-  setTimeout(()=>{
-    bgDark.style.display = "none"
-  }, 50)
-}) */
